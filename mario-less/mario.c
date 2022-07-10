95086b624ebe6cfd2a0e77f14a91ba0862dd342f
Yael <66230691+yeye11117@users.noreply.github.com>
2022-07-10 03:28:07 +0000

    Sun, Jul 10, 2022, 3:28 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-    int block = 0;
+    int userNum = 0;
 
     printf("Type a number between 1 and 8: \n");
-    scanf ("%d", block);
-    printf("Your number is: %d \n", block);
+    scanf ("%d", &userNum);
+    printf("Your number is: %d \n", userNum);
 
 }
