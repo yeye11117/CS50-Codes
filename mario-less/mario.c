d1677339d4b7c303f9152982a68b84d9e65eb40d
Yael <66230691+yeye11117@users.noreply.github.com>
2022-07-10 03:37:02 +0000

    Sun, Jul 10, 2022, 3:37 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,10 +3,17 @@
 
 int main(void)
 {
-    int blockHeight;
+    int blockHeight, printer;
 
     printf("Enter a height number between 1 and 8: \n");
     scanf ("%d", &blockHeight);
+
+    for(printer = 1, printer <= blockHeight; ++printer);
+    {
+
+        printf("kekekekek");
+
+    }
     printf("Your number is: %d \n", blockHeight);
 
 }
