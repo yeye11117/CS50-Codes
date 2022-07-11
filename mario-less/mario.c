b0e28603d71f0e2ce7cfde1be463c5d8165db78d
Yael <66230691+yeye11117@users.noreply.github.com>
2022-07-11 05:32:28 +0000

    Mon, Jul 11, 2022, 5:32 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -13,7 +13,7 @@ int main(void)
 
     while(row < 1 || row > 8);
 
-        for(y = 1; y <= row; ++y)
+        for(y = 1; y < row; ++y)
         {
 
             for(space = 0; space < y - row - 1; space++)
