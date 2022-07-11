57b9318b8b8aa111bcbc788153c265888e87ae70
Yael <66230691+yeye11117@users.noreply.github.com>
2022-07-11 16:15:23 +0000

    Mon, Jul 11, 2022, 4:15 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,34 +4,34 @@
 int main(void)
 {
     int y, x, row, space;
-
+    //Takes info from user
     do
     {
         printf("How many rows between 1 and 8: \n");
-        scanf ("%d", &row);
+        scanf("%d", &row);
     }
-
+    //If user inputs anything less than 1 or more than 8, it will return true and loop back
     while(row < 1 || row > 8);
-
-        for(y = 1; y <= row; ++y)
+    //this prints the number of rows
+    for(y = 1; y <= row; ++y)
+    {
+        //this prints the spaces
+        for(space = 0; space < row - y; space++)
         {
 
-            for(space = 0; space < row - y; space++)
-            {
-
-                printf(" ");
-
-            }
-
-            for(x = 1; x <= y; ++x)
-            {
-
-                printf("#");
-
-            }
-
-            printf(" \n");
+            printf(" ");
 
         }
+        //this prints the hashtag
+        for(x = 1; x <= y; ++x)
+        {
+
+            printf("#");
+
+        }
+
+        printf("\n");
+
+    }
 
 }
