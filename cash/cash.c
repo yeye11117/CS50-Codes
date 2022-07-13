29889e7c8ec29294ecad5b26dae2c512eb2035ed
Yael <66230691+yeye11117@users.noreply.github.com>
2022-07-13 01:11:13 +0000

    Wed, Jul 13, 2022, 1:11 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -52,24 +52,44 @@ int get_cents(void)
 
 int calculate_quarters(int cents)
 {
-    for()
-    return 0;
+    int quarters = 0;
+    while(cents >= 25)
+    {
+        cents = cents - 25;
+        quarters++;
+    }
+    return quarters;
 }
 
 int calculate_dimes(int cents)
 {
-
-    return 0;
+    int dimes = 0;
+    while(cents >= 10)
+    {
+        cents = cents - 10;
+        dimes++;
+    }
+    return dimes;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
-    return 0;
+    int nickels = 0;
+    while(cents >= 5)
+    {
+        cents = cents - 5;
+        nickels++;
+    }
+    return nickels;
 }
 
 int calculate_pennies(int cents)
 {
-    // TODO
-    return 0;
+    int pennies = 0;
+    while(cents >= 1)
+    {
+        cents = cents - 1;
+        pennies++;
+    }
+    return pennies;
 }
