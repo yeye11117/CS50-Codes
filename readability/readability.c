75664ab62c46a8c07122c1a0808de8404d5081a4
Yael <66230691+yeye11117@users.noreply.github.com>
2022-07-29 00:35:07 +0000

    Fri, Jul 29, 2022, 12:35 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,13 +5,18 @@
 
 int main(void)
 {
+    
     string sent = get_string("Text: ");
     int number = count_letters(sent);
     printf("%d", text);
 
 }
 
-int count_letters(string text)
+
+
+
+
+/*int count_letters(string text)
 {
     int count = 0;
     for(int i = 0; i < strlen(text); i++)
@@ -22,5 +27,4 @@ int count_letters(string text)
         }
     }
     return count;
-
-}
+} */
