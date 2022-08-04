1cf5e05b34de18741bc50231a64b9e581d666bfb
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-04 00:45:35 +0000

    Thu, Aug 4, 2022, 12:45 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -34,5 +34,10 @@ int main(void)
     printf("%d words \n", words);
     printf("%d sentence \n", sentence);
 
-    index = 0.0588 * L - 0.296 * S - 15.8
+    /*index = 0.0588 * L - 0.296 * S - 15.8
+    where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.*/
+
+    int l = letters/100;
+
+
 }
