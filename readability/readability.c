4e6709d78f917af3ff4d90dce03da0db56de898d
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-04 00:47:12 +0000

    Thu, Aug 4, 2022, 12:47 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -37,7 +37,8 @@ int main(void)
     /*index = 0.0588 * L - 0.296 * S - 15.8
     where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.*/
 
-    int l = letters/100;
-
+    int m = words*100;
+    int l = letters/m;
+    int s =
 
 }
