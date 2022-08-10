d022eb9fe81ee1e7f713697db48ace517199fe30
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-10 03:01:06 +0000

    Wed, Aug 10, 2022, 3:01 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -15,36 +15,43 @@ int main(void)
 
     for(int i = 0; i < strlen(text); i++)
     {
+        /* Counts letters, words and sentences*/
         if (isalnum(text[i]))
         {
             letters++;
         }
 
-        else if( text[i] == ' ')
+        else if (text[i] == ' ')
         {
             words++;
         }
 
-        else if(text[i] == '.' || text[i] == '!' || text[i] == '?'  )
+        else if(text[i] == '.' || text[i] == '!' || text[i] == '?')
         {
             sentence++;
         }
 
     }
-    printf("%f letters \n", letters);
-    printf("%f words \n", words);
-    printf("%f sentence \n", sentence);
 
-    /*index = 0.0588 * L - 0.296 * S - 15.8
-    where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.*/
+    /*index = 0.0588 * L - 0.296 * S - 15.8*/
+    /*where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.*/
 
     float l = (letters/words)*100;
-    printf("%f \n", l);
     float s = (sentence/words)*100;
-    printf("%f \n", s);
-    float index = (0.0588 * l) - (0.296 * s) - 15.8;
-    printf("%f \n", round(index));
+    /*We convert the float into an int, otherwise it will print out a float number for the index which throws it off*/
+    int index = round((0.0588 * l) - (0.296 * s) - 15.8);
 
-
-
+    if(index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if(index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        /*Here is the reasn why we convert float to int.*/
+        printf("Grade %i\n", index);
+    }
 }
