ce587774382defaa1748dca7a5001041e9ea8bb3
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-14 19:31:02 +0000

    Sun, Aug 14, 2022, 7:31 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,12 +6,13 @@
 
 int main(int argc, string argv[])
 {
+    //This part of the code ensures that there only two arguments on the terminal "./Caesar + key"
     if (argc != 2)
     {
         printf("Usage: ./caesar key");
         return 1;
     }
-
+    //This ensures that the argument is in fact a digit by comparing it to a numerical value that loops around to match the argument.
     for (int i = 0; i < strlen(argv[1]); i++)
     {
         if(!isdigit(argv[1][i]))
@@ -21,6 +22,7 @@ int main(int argc, string argv[])
         }
 
         int k = atoi(argv[1]);
+        string plaintext = get_string
     }
 
 
