5bf792cc5bc49d9cf8022c45a5805381165681b8
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-14 20:10:00 +0000

    Sun, Aug 14, 2022, 8:10 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -30,8 +30,8 @@ int main(int argc, string argv[])
         {
             if(isupper(plaintext[j]))
             {
-            // %C prints character individually, character ascii value - 65 to bring it to 0 + cipher x mod26 loops the alphabet + 65 to bring it into ascii value
-            printf(("%c", plaintext[j] - 65 + k) % 26 + 65);\
+                // %C prints character individually, character ascii value - 65 to bring it to 0 + cipher x mod26 loops the alphabet + 65 to bring it into ascii value
+                printf(("%c", plaintext[j] - 65 + k) % 26 + 65);\
             }
 
         }
