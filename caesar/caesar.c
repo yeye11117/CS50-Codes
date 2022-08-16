122894ba8e4fb30ceedeb1b39266a7cd05ee6442
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-16 03:45:04 +0000

    Tue, Aug 16, 2022, 3:45 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,16 +9,21 @@ int main(int argc, string argv[])
     //This part of the code ensures that there only two arguments on the terminal "./Caesar + key"
     if (argc != 2)
     {
+
         printf("Usage: ./caesar key");
         return 1;
+
     }
     //This ensures that the argument is in fact a digit by comparing it to a numerical value that loops around to match the argument.
     for (int i = 0; i < strlen(argv[1]); i++)
+
     {
         if(!isdigit(argv[1][i]))
         {
+
             printf("Usage: ./caesar key");
             return 1;
+
         }
         //We need to turn our argument from a string to an int so we can later do math with it
         int k = atoi(argv[1]);
@@ -28,15 +33,21 @@ int main(int argc, string argv[])
 
         for (int j = 0; j < strlen(plaintext); j++)
         {
-            if (isupper(plaintext[j]))
+
+            if(isupper(plaintext[j]))
             {
                 // %C prints character individually, character ascii value - 65 to bring it to 0 + cipher x mod26 loops the alphabet + 65 to bring it into ascii value
-                printf(("%c", plaintext[j] - 65 + k) %26 + 65);
+                printf(("%c", plaintext[j] - 65 + k) % 26 + 65);
+
+            }
+
+            else if(islower(plaintext[j]))
+            {
+
+                printf(("%c", plaintext[j] - 97 + k) % 26 + 97);
+
             }
 
         }
-
-
-
 
 }
