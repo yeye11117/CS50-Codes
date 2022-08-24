fff8abced2ad67368b49947880d189630fa5140f
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-24 22:41:09 +0000

    Wed, Aug 24, 2022, 10:41 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,14 +81,23 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+
     int winner = 0;
 
     for (int i = 0; i < candidate_count; i++)
     {
+
         if(candidates[i].votes > winner)
         {
-            
+
+            winner = candidates[i].votes;
+
         }
+
+        
+
     }
+
     return;
+
 }
