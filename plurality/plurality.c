1e8a96145ea126e6f9866e9a01667040b6fbca9d
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-24 19:59:20 +0000

    Wed, Aug 24, 2022, 7:59 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -65,7 +65,7 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; i < candidate_name; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
 
     }
