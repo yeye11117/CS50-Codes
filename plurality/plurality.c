b3ff22fc82061ccdee073cac0f9c19eae8a74943
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-24 20:01:39 +0000

    Wed, Aug 24, 2022, 8:01 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -67,7 +67,10 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
+        if(strcmp(candidates[i].name, name) == 0)
+        {
 
+        }
     }
     return false;
 }
