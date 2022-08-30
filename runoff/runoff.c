72626492b099688adfe58387939f59cb92903ed9
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-30 00:43:42 +0000

    Tue, Aug 30, 2022, 12:43 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -152,7 +152,10 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    if()
+    if(int i = 0; i < voter_count; i++)
+    {
+        
+    }
     return;
 }
 
