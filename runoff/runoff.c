851774f69161478b8519afffe5151a0b2a472b47
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-30 00:26:56 +0000

    Tue, Aug 30, 2022, 12:26 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,9 +128,9 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for(int i = 0; i < voter_count; i++)
+    for(int i = 0; i < candidate_count; i++)
     {
-        if(strcmp(name, candidates[i].name)==0)
+        if(strcmp(name, candidates[i].name) == 0)
         {
             prefrences[voter][rank] = i;
             return true;
