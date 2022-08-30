fd9ba2adfd4e9e7f66b6762f4d6fb184c63edd20
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-30 22:17:47 +0000

    Tue, Aug 30, 2022, 10:17 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -161,21 +161,16 @@ bool print_winner(void)
 {
     for(int i = 0; i < candidate_count; i++)
     {
-
         if(candidate_count >= candidate_count/2)
         {
             printf("some one won");
             return true;
         }
-
-        else
-        {
-            return false;
-        }
+        return false;
 
     }
-}
 
+}
 
 
 
