5cc5307cb57931cdd794de728cb1bb544dff42b5
Yael <66230691+yeye11117@users.noreply.github.com>
2022-08-30 00:30:43 +0000

    Tue, Aug 30, 2022, 12:30 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,12 +145,28 @@ bool vote(int voter, int rank, string name)
 
 
 
+
+
+
+
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
     // TODO
     return;
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
