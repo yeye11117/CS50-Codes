a8232c07c611e763fe8c661269fc207c4fa78cc7
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-03 04:31:06 +0000

    Sat, Sep 3, 2022, 4:31 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -146,7 +146,7 @@ void tabulate(void)
     {
         for(int j = 0; j < candidate_count; j++)
         {
-            if(candidates[preferences[i][j]].eliminated == true)
+            if(candidates[i].eliminated == false)
             {
                 candidates[preferences[i][j]].votes++;
                 break;
