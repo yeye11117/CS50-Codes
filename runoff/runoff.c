40e370c6acecdc7153c12852de493df8d3c24f16
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-04 01:02:09 +0000

    Sun, Sep 4, 2022, 1:02 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -161,11 +161,10 @@ bool print_winner(void)
 {
     for(int i = 0; i < candidate_count; i++)
     {
-        string most = candidates[i].name;
 
         if(candidates[i].votes > voter_count/2)
         {
-            printf("Winner %s\n", most);
+            printf("%s\n", candidates[i].name);
             return true;
         }
 
