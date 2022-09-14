b3d82540c94664c5063af6414c27d73702256412
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-14 20:43:45 +0000

    Wed, Sep 14, 2022, 8:43 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,7 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t HEADER[HEADER_SIZE];
-    fread(HEADER, sizeof(uint8_t))
+    fread(HEADER, sizeof(uint8_t),HEADER_SIZE, input);
+    fwrite(HEADER, sizeof(uint8_t),HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
 
