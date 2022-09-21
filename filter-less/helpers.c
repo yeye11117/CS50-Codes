9c8731dbeaeec5c107e9e989571ab2fc5893e53f
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-21 01:04:15 +0000

    Wed, Sep 21, 2022, 1:04 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -29,6 +29,25 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    //comb through every column
+    for (int i = 0; i < height; i++)
+    {
+        //comb through every row
+        for (int j = 0; j < width; j++)
+        {
+
+            //convert pixels to float
+            float Red = image[i][j].rgbtRed;
+            float Blue = image[i][j].rgbtBlue;
+            float Green = image[i][j].rgbtGreen;
+
+            //find the average value
+            int average = round((Red + Blue + Green) / 3);
+            image[i][j].rgbtRed = image[i][j].rgbtBlue = image[i][j].rgbtGreen = average;
+
+        }
+
+    }
     return;
 }
 
