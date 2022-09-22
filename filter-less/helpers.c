46c00e53233fe112026f10810f4cbf953d02f350
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-22 00:20:42 +0000

    Thu, Sep 22, 2022, 12:20 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -76,7 +76,19 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+
+    for (int i = 0; i < height; i++)
+    {
+
+        for (int j = 0; j < width/2; j++)
+        {
+
+            
+
+        }
+
+    }
+
     return;
 }
 
