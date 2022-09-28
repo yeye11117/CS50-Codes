47bcdd1f13dcff98107ff6f399334d4004dc0157
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-28 18:39:51 +0000

    Wed, Sep 28, 2022, 6:39 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -80,7 +80,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
     {
 
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < width / 2; j++)
         {
 
             RGBTRIPLE temp = image[i][j];
@@ -142,9 +142,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
 
                 //calculate average pixels
-                temp[i][j].rgbtRed = round(totalRed/ counter);
-                temp[i][j].rgbtBlue = round(totalBlue/ counter);
-                temp[i][j].rgbtGreen = round(totalGreen/ counter);
+                temp[i][j].rgbtRed = round(totalRed / counter);
+                temp[i][j].rgbtBlue = round(totalBlue / counter);
+                temp[i][j].rgbtGreen = round(totalGreen / counter);
 
             }
         }
