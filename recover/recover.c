7ccff141f6db4a56339601a7c8100bf45dd43c36
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-28 21:03:26 +0000

    Wed, Sep 28, 2022, 9:03 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +11,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *input_file = fopen(argv[1], r);
+    FILE *input_file = fopen(argv[1], "r");
 
     // check that the input_file is valid.
 
@@ -18,5 +20,18 @@ int main(int argc, char *argv[])
         printf("Could not open file");
         return 2;
     }
-    
+
+    //store bocks
+    unsigned char buffer[512];
+
+    //track number of images
+    int count_image = 0;
+
+    //file pointer for recovered images
+    FILE *output_file = NULL;
+
+    //char filename
+    char *filename = malloc(8 * sizeof(char));
+
+    fread (buffer, sizeof(char), 512)
 }
