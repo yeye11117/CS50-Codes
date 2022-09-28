bc33b758d1b5a20e6a9290cafdbd002a41c4cab8
Yael <66230691+yeye11117@users.noreply.github.com>
2022-09-28 20:45:18 +0000

    Wed, Sep 28, 2022, 8:45 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    FILE *input_file = fopen(argv[1], r);
+
+    // check that the inout_file is valid.
+
+    
+
 }
