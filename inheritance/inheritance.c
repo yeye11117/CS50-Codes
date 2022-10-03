bb6e6d4ae8d41a37fe03232a4bc8404f98f937db
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-03 23:15:34 +0000

    Mon, Oct 3, 2022, 11:15 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -73,17 +73,26 @@ person *create_family(int generations)
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+
+        return;
+
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
+    free(p);
 
 }
 
