8581fdd99078b58e08f941a33eda76afbfe0026e
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-03 23:16:54 +0000

    Mon, Oct 3, 2022, 11:16 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -54,8 +54,8 @@ person *create_family(int generations)
         p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = p->parents[0]->alleles[rand()%2];
-        p->alleles[1] = p->parents[1]->alleles[rand()%2];
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2];
+        p->alleles[1] = p->parents[1]->alleles[rand() % 2];
 
     }
 
