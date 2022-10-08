c86993c739ee84bded21951b375c005aadd7af24
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-08 18:50:19 +0000

    Sat, Oct 8, 2022, 6:50 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -7,3 +7,8 @@ while True:
 
 for i in range(height):
     for j in range(height):
+        if (i + j < height - 1):
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()
