50580d6156f6f73a3087c9e109b20317b30c82f0
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-08 18:14:46 +0000

    Sat, Oct 8, 2022, 6:14 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,9 +1,11 @@
 height = int(input("Height: "))
 
-if height > 0 or height < 9:
+if height >= 1 or height <= 8:
 
     for y in range(height):
         print("Q")
         for x in range(height):
             print("#", end = '')
     #break
+else:
+    continue
