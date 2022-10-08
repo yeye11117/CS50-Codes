9f038f88b5f64bceab5ed0c59119174ea1f74439
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-08 18:27:11 +0000

    Sat, Oct 8, 2022, 6:27 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,7 +3,7 @@ import cs50
 height = cs50.get_int("Height: ")
 
 
-while height >= 1 or height <= 8:
+while height not (height >= 1 or height <= 8):
     for y in range(height):
         print("Q")
         for x in range(height):
