3cc5ddb5180af255fe660a232371ef67b946a0e2
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-08 16:12:01 +0000

    Sat, Oct 8, 2022, 4:12 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -60,7 +60,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    while teams > 1:
+    while len(teams > 1):
         teams = simulate_rounds(teams)
     return teams[0]["teams"]
 
