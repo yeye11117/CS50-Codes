1f5272237fe92c2f00fa424983b129bb1ba667a0
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-08 18:17:43 +0000

    Sat, Oct 8, 2022, 6:17 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,11 +1,8 @@
 height = int(input("Height: "))
 
 while height >= 1 or height <= 8:
-
     for y in range(height):
         print("Q")
         for x in range(height):
             print("#", end = '')
     break
-
-    continue
