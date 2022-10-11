6fffe71175ff90da52b6bf4b11fd3893d30c2943
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-11 19:44:51 +0000

    Tue, Oct 11, 2022, 7:44 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,11 +1,9 @@
 from cs50 import get_float
 
+def cal_quarter(dollar):
+    
+
 while True:
-    cent = get_float("Change owed: ")
-    if cent >= 1:
+    dollar = get_float("Change owed: ")
+    if dollar >= 1:
         break
-
-#def main():
-
-#if __name__ == ("__main__"):
-   # main()
