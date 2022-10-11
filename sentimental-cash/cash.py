8c3081a7be9601c44a881ed07967fad271d7fc70
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-11 19:00:39 +0000

    Tue, Oct 11, 2022, 7:00 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -3,8 +3,19 @@ from cs50 import get_float
 x = get_float("Change owed: ")
 
 def main():
+    get_cents(cent)
 
-    def get_cents
+
+
+
+
+
+
+
+def get_cents(cent):
+    user = get_float("Change owed: ")
+    return cent
+
 
 
 if __name__ == ("__main__"):
