c15845ae940e99a583d5b7f905b2d9e456050a50
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-11 22:15:39 +0000

    Tue, Oct 11, 2022, 10:15 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -9,6 +9,23 @@ def cal_quarter(dollar):
         print(dollar)
     return quarter
 
+# There is a rounding issue here due to the float variabel. Fix later
+#Change owed: 1.33
+#1.08
+#0.8300000000000001
+#0.5800000000000001
+#0.33000000000000007
+#0.08000000000000007
+#5
+
+def cal_dimes(dollar):
+    dimes = 0
+    while dollar >= 0.10:
+        dollar = dollar - 0.10
+        dimes = dimes + 1
+        print(dollar)
+    return dimes
+
 while True:
     dollar = get_float("Change owed: ")
     if dollar >= 1:
