646eb8f87e10ee17ed1a7c69b7df5cce2e117add
Yael <66230691+yeye11117@users.noreply.github.com>
2022-10-11 23:58:37 +0000

    Tue, Oct 11, 2022, 11:58 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,5 +1,9 @@
 from cs50 import get_float
 
+while True:
+    dollar = get_float("Change owed: ")
+    if dollar >= 1:
+        break
 
 def cal_quarter(dollar):
     quarter = 0
@@ -9,15 +13,6 @@ def cal_quarter(dollar):
         print(dollar)
     return quarter
 
-# There is a rounding issue here due to the float variabel. Fix later
-#Change owed: 1.33
-#1.08
-#0.8300000000000001
-#0.5800000000000001
-#0.33000000000000007
-#0.08000000000000007
-#5
-
 def cal_dimes(dollar):
     dimes = 0
     while dollar >= 0.10:
@@ -26,14 +21,6 @@ def cal_dimes(dollar):
         print(dollar)
     return dimes
 
-while True:
-    dollar = get_float("Change owed: ")
-    if dollar >= 1:
-        break
-
-test = cal_quarter
-
-
-#x = cal_quarter(dollar)
+x = cal_quarter(dollar)
 y = cal_dimes(dollar)
 print(y)
