07e6c7694dccd98c76ae3aa2a79865316549e4db
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-01 21:55:12 +0000

    Tue, Nov 1, 2022, 9:55 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,4 +1,6 @@
 --In 7.sql, write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating. For movies with the same rating, order them alphabetically by title.
 
 SELECT title, ratings
-JOIN ratings ON movie_id = 
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE year = 2010
+ORDER BY rating DESC, title ASC;
---
+++ movies/8.sql
@@ -0,0 +1,3 @@
+--In 8.sql, write a SQL query to list the names of all people who starred in Toy Story.
+
+SELECT 
