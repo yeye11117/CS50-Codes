a3bdcb74621192ae7200fcf85af62f4b04253309
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 04:14:46 +0000

    Wed, Nov 2, 2022, 4:14 AM +00:00

---
+++ movies/11.sql
@@ -0,0 +1,8 @@
+SELECT title
+FROM movies
+JOIN ratings ON stars.movie_id = movies.id
+JOIN stars ON stars.movie_id = movies.id
+JOIN people ON stars.person_id = people.id
+WHERE people.name = "Chadwick Boseman"
+ORDER BY ratings.rating DESC
+LIMIT 5;
--- movies/9.sql
+++ movies/9.sql
@@ -1,2 +1,7 @@
 --In 9.sql, write a SQL query to list the names of all people who starred in a movie released in 2004, ordered by birth year.
 
+SELECT DISTINCT name FROM people
+JOIN stars on stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE movie.year = 2004
+ORDER BY people.birth ASC;
