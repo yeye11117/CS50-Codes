734c70f745b5a5252fc3cf3593c3c32a24476553
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 21:47:43 +0000

    Wed, Nov 2, 2022, 9:47 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,5 +9,11 @@ WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE
 
 SELECT bakery_security_logs.activity, bakery_security_logs.
 license_plate, people.name FROM people
-JOIN bakery_security_logs ON bakery_securoty_logs.license_plate =
+JOIN bakery_security_logs ON bakery_security_logs.license_plate =
 people.license_plate
+WHERE bakery_security_logs.year = 2021
+AND bakery_security_logs.month = 7
+AND bakery_security_logs.day = 28
+AND bakery_security_logs.hour = 10
+AND bakery_security_logs.minute >= 15
+AND bakery_security_logs.minute <= 25;
