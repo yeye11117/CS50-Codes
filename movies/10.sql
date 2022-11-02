0180e8fc146df7c43a3e4776c3758a619dfc88d5
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 04:34:05 +0000

    Wed, Nov 2, 2022, 4:34 AM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,7 +1,7 @@
---In 10.sql, write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0. 
+--In 10.sql, write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
 SELECT DISTINCT name
 FROM people
 JOIN directors ON directors.person_id = people.id
 JOIN movies on movies.id = directors.movie_id
-JOIN raitings on raitings.movie_id = movies.id
+JOIN ratings on ratings.movie_id = movies.id
 WHERE rating >= 9.0;
