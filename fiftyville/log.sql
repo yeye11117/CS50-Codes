11131214d2fbd30631680f5a116d813a08fedeeb
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 21:55:48 +0000

    Wed, Nov 2, 2022, 9:55 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -21,3 +21,9 @@ AND bakery_security_logs.minute <= 25;
 SELECT people.name, atm_transactions.transacion_type FROM people
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number =
+bank_accounts.accpunt_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_location = "Leggett Street"
+AND atm_transactions.transaction_type = "withdraw";
