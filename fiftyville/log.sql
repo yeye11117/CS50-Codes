154f79ed7b274bb349bd64081da7f80f8bf8ccc5
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 22:06:01 +0000

    Wed, Nov 2, 2022, 10:06 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -44,3 +44,10 @@ AND month = 7
 AND day = 28
 AND duration < 68;
 
+SELECT id, hour, minute, origin_airport_id, destination_airport_id
+FROM flights
+WHERE year = 2021
+AND month = 7
+AND day = 29
+ORDER BY hour ASC
+LIMIT 1;
