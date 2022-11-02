6c85999eece3a6aec7a321b97889c7c73ed8319a
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 04:47:33 +0000

    Wed, Nov 2, 2022, 4:47 AM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,8 +1,7 @@
 --In 11.sql, write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated
 
-SELECT title
-FROM movies
-JOIN ratings ON stars.movie_id = movies.id
+SELECT title FROM movies
+JOIN ratings ON ratings.movie_id = movies.id
 JOIN stars ON stars.movie_id = movies.id
 JOIN people ON stars.person_id = people.id
 WHERE people.name = "Chadwick Boseman"
