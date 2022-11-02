1835d0a58663cb28488d58696e2368d086c78e11
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 01:00:13 +0000

    Wed, Nov 2, 2022, 1:00 AM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,4 +2,5 @@
 
 SELECT name FROM people
 JOIN stars ON stars.person_id = people.id
-JOIN movies ON movies.id = stars.movie_id;
+JOIN movies ON movies.id = stars.movie_id
+WHERE movies.title = "Toy Story";
