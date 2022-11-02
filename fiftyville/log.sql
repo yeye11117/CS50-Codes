1f22d60a403c3a50a8f2cd4138dbefbf4c24017a
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 22:19:31 +0000

    Wed, Nov 2, 2022, 10:19 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -59,4 +59,30 @@ license_plate from people
 JOIN passengers ON people.passport_number = passengers.
 passport_number
 JOIN flights ON flights.id = passengers.flight_id
-WHERE (flights.year)
+WHERE (flights.year = 2021 AND flights.month = 7 AND flights.day =
+29 AND flights.id = 36)
+AND name IN
+(SELECT phone_calls.caller_name FROM phone_calls
+WHERE year = 2021
+AND month = 7
+AND day = 28
+AND duration < 60)
+AND name IN
+(SELECT people.name FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number =
+bank_accounts.account_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_location = "Leggett Street"
+AND atm_transactions.transaction_type = "withdraw")
+AND name IN
+(JOIN bakery_security_logs ON bakery_security_logs.license_plate =
+people.license_plate
+WHERE bakery_security_logs.year = 2021
+AND bakery_security_logs.month = 7
+AND bakery_security_logs.day = 28
+AND bakery_security_logs.hour = 10
+AND bakery_security_logs.minute >= 15
+AND bakery_security_logs.minute <= 25);
