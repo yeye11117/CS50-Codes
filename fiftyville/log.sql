ac9615d0b0a757f7a71304ce4719dad1e37a6de1
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-02 21:44:55 +0000

    Wed, Nov 2, 2022, 9:44 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,3 +6,8 @@ WHERE year = 2021 AND month = 7 AND day = 28;
 SELECT transcript FROM interviews
 WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE
 "%bakery%";
+
+SELECT bakery_security_logs.activity, bakery_security_logs.
+license_plate, people.name FROM people
+JOIN bakery_security_logs ON bakery_securoty_logs.license_plate =
+people.license_plate
