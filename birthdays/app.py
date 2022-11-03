2c01f64938177849ff0ff7ee929fdf128a88690b
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-03 23:33:55 +0000

    Thu, Nov 3, 2022, 11:33 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,6 +33,6 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        registrants = db.execute("SELECT * FROM birthdays")
+        people = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", people = people)
 
