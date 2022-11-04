4b10bc837ee446e0d299f7a37be1b6ce830cd213
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-04 21:30:22 +0000

    Fri, Nov 4, 2022, 9:30 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -72,7 +72,7 @@ def buy():
         transaction_value = shares * stock["price"]
 
         user_id = session["user_id"]
-        user_cash_db = db.execute("SELECT cash FROM users WHERE id = :id, id=user_id")
+        user_cash_db = db.execute("SELECT cash FROM users WHERE id = ?)
         user_cash = user_cash_db[0]["cash"]
 
         if user_cash < transaction_value:
