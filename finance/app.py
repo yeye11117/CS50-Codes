bb26821c25a277e499456e321b8b99646a267e89
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-04 19:45:31 +0000

    Fri, Nov 4, 2022, 7:45 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -111,7 +111,16 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    if request.method == "GET":
+        return render_template("quote.html")
+
+    else:
+        symbol = request.form.get("symbol")
+
+        if not symbol:
+            return apology("Must Give Symbol")
+
+        
 
 
 @app.route("/register", methods=["GET", "POST"])
