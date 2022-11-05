b6d290958760c2e7cc599f8d05b2dffa4af1d8b2
Yael Perez <66230691+yeye11117@users.noreply.github.com>
2022-11-05 01:03:31 +0000

    Sat, Nov 5, 2022, 1:03 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -96,6 +96,22 @@ def buy():
 
         return redirect("/")
 
+@app.route("/add_cash", methods = ["GET", "POST"])
+@login_required
+def add_cash():
+    """User can add money"""
+    if request.method == "GET":
+        return render_template("add.html")
+    else:
+        new_cash = request.form.get("new_cash")
+
+        if not new_cash:
+            return apology("You must provide a card")
+
+        user_id = session["user_id"]
+
+        
+
 @app.route("/history")
 @login_required
 def history():
